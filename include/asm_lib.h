e6c1ccb8731b2e5a5fcd802fcc964fbed968feb1
Zibgame <fanizicy@gmail.com>
2025-11-17 16:43:04 +0100

    Start dev Write

--- include/asm_lib.h
+++ include/asm_lib.h
@@ -6,14 +6,16 @@
 /*   By: zcadinot <zcadinot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/17 16:24:34 by zcadinot          #+#    #+#             */
-/*   Updated: 2025/11/17 16:24:52 by zcadinot         ###   ########.fr       */
+/*   Updated: 2025/11/17 16:42:10 by zcadinot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASM_LIB_H
 #define ASM_LIB_H
 
-void asm_write_char(char c);
+typedef unsigned long size_t;
+typedef long ssize_t;
+
+ssize_t	write(int fd, const void *buf, size_t count);
 
 #endif
-
